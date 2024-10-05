017410aa8e3706f0fd9daf0f631e5849cc1c8559
James Anderson <james.anderson2@btinternet.com>
2024-10-05 21:58:33 +0100

    Load single company

--- SharePicker/SharePicker/Services/CompanyProvider.cs
+++ SharePicker/SharePicker/Services/CompanyProvider.cs
@@ -10,30 +10,38 @@ public class CompanyProvider(FmpClient fmpClient) : BackgroundService
 
     protected override async Task ExecuteAsync(CancellationToken cancellationToken)
     {
-        var tradableCompanies = await fmpClient.GetTradableCompaniesAsync(cancellationToken);
-        var symbolsWithFinancialStatements = await fmpClient.GetSymbolsWithFinancialStatementsAsync(cancellationToken);
-        
-        var companies = new List<Company>();
-        foreach (var company in tradableCompanies
-            .Where(company => symbolsWithFinancialStatements.Contains(company.Symbol)))
+        while (!cancellationToken.IsCancellationRequested)
         {
-            if (company.ExchangeShortName == null)
-                continue;
+            using var timer = new PeriodicTimer(TimeSpan.FromHours(1));
+            var tradableCompanies = await fmpClient.GetTradableCompaniesAsync(cancellationToken);
+            var symbolsWithFinancialStatements = await fmpClient.GetSymbolsWithFinancialStatementsAsync(cancellationToken);
 
-            var balanceSheetStatements = await fmpClient.GetBalanceSheetStatementsAsync(company.Symbol, cancellationToken);
-            var cashFlowStatements = await fmpClient.GetCashFlowStatementsAsync(company.Symbol, cancellationToken);
-            var incomeStatements = await fmpClient.GetIncomeStatementsAsync(company.Symbol, cancellationToken);
-            var ratios = await fmpClient.GetRatiosAsync(company.Symbol, cancellationToken);
-            companies.Add(new Company(
-                company.Symbol,
-                company.Name, 
-                company.ExchangeShortName,
-                balanceSheetStatements.Select(statement => statement.ToDomain()).ToList(),
-                cashFlowStatements.Select(statement => statement.ToDomain()).ToList(),
-                incomeStatements.Select(statement => statement.ToDomain()).ToList(),
-                ratios.Select(statement => statement.ToDomain()).ToList()));
+            var companies = new List<Company>();
+            foreach (var company in tradableCompanies
+                .Where(company => symbolsWithFinancialStatements.Contains(company.Symbol)))
+            {
+                if (company.ExchangeShortName == null || company.ExchangeShortName != "NYSE")
+                    continue;
+
+                var balanceSheetStatements = await fmpClient.GetBalanceSheetStatementsAsync(company.Symbol, cancellationToken);
+                var cashFlowStatements = await fmpClient.GetCashFlowStatementsAsync(company.Symbol, cancellationToken);
+                var incomeStatements = await fmpClient.GetIncomeStatementsAsync(company.Symbol, cancellationToken);
+                var ratios = await fmpClient.GetRatiosAsync(company.Symbol, cancellationToken);
+                companies.Add(new Company(
+                    company.Symbol,
+                    company.Name,
+                    company.ExchangeShortName,
+                    balanceSheetStatements.Select(statement => statement.ToDomain()).ToList(),
+                    cashFlowStatements.Select(statement => statement.ToDomain()).ToList(),
+                    incomeStatements.Select(statement => statement.ToDomain()).ToList(),
+                    ratios.Select(statement => statement.ToDomain()).ToList()));
+
+                break;
+            }
+
+            _companies = companies;
+
+            await timer.WaitForNextTickAsync(cancellationToken);
         }
-
-        _companies = companies;
     }
 }
