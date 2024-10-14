59282c614b652e4e659ff79c248a7fcc898ccb4c
James Anderson <james.anderson2@btinternet.com>
2024-10-14 21:37:54 +0100

    Inline mapping functions

--- SharePicker/SharePicker/Services/DatabaseWriter.cs
+++ SharePicker/SharePicker/Services/DatabaseWriter.cs
@@ -52,45 +52,38 @@ public class DatabaseWriter(
         var company = await dbContext.Companies.SingleOrDefaultAsync(company => company.Symbol == stock.Symbol, cancellationToken)
             ?? new Company { Name = stock.Name, Symbol = stock.Symbol, Exchange = exchange };
 
-        foreach (var incomeStatement in await fmpClient
-                    .GetIncomeStatementsAsync(stock.Symbol, cancellationToken))
+        foreach (var dto in await fmpClient.GetIncomeStatementsAsync(stock.Symbol, cancellationToken))
         {
             await dbContext.IncomeStatements.AddAsync(
-                await ToDatabaseObject(dbContext, incomeStatement, company, cancellationToken),
+                new IncomeStatement
+                {
+                    Company = company,
+                    Date = DateOnly.ParseExact(dto.Date, "yyyy-MM-dd"),
+                    Currency = await GetCurrency(dbContext, dto.ReportedCurrency, cancellationToken),
+                    Revenue = dto.Revenue,
+                    CostOfSales = dto.CostOfRevenue,
+                    GrossProfit = dto.GrossProfit,
+                    ResearchAndDevelopmentCosts = dto.ResearchAndDevelopmentExpenses,
+                    DistributionCosts = dto.SellingAndMarketingExpenses,
+                    AdministrativeCosts = dto.GeneralAndAdministrativeExpenses,
+                    OtherCosts = dto.OtherExpenses,
+                    OperatingProfit = dto.OperatingIncome,
+                    ProfitBeforeIncomeAndTaxation = dto.IncomeBeforeTax - dto.InterestIncome + dto.InterestExpense,
+                    FinanceIncome = dto.InterestIncome,
+                    FinanceExpense = dto.InterestExpense,
+                    ProfitBeforeTax = dto.IncomeBeforeTax,
+                    Taxation = dto.IncomeTaxExpense,
+                    ProfitAfterTax = dto.IncomeBeforeTax - dto.IncomeTaxExpense,
+                    NetProfit = dto.NetIncome,
+                    EarningsPerShare = dto.Eps,
+                    EarningsPerShareDiluted = dto.EpsDiluted
+                },
                 cancellationToken);
         }
 
         await dbContext.SaveChangesAsync(cancellationToken);
     }
-
-    private static async Task<IncomeStatement> ToDatabaseObject(
-        SharePickerDbContext dbContext,
-        IncomeStatementDto dto,
-        Company company,
-        CancellationToken cancellationToken) =>
-        new IncomeStatement
-        {
-            Company = company,
-            Date = DateOnly.ParseExact(dto.Date, "yyyy-MM-dd"),
-            Currency = await GetCurrency(dbContext, dto.ReportedCurrency, cancellationToken),
-            Revenue = dto.Revenue,
-            CostOfSales = dto.CostOfRevenue,
-            GrossProfit = dto.GrossProfit,
-            ResearchAndDevelopmentCosts = dto.ResearchAndDevelopmentExpenses,
-            DistributionCosts = dto.SellingAndMarketingExpenses,
-            AdministrativeCosts = dto.GeneralAndAdministrativeExpenses,
-            OtherCosts = dto.OtherExpenses,
-            OperatingProfit = dto.OperatingIncome,
-            ProfitBeforeIncomeAndTaxation = dto.IncomeBeforeTax - dto.InterestIncome + dto.InterestExpense,
-            FinanceIncome = dto.InterestIncome,
-            FinanceExpense = dto.InterestExpense,
-            ProfitBeforeTax = dto.IncomeBeforeTax,
-            Taxation = dto.IncomeTaxExpense,
-            ProfitAfterTax = dto.IncomeBeforeTax - dto.IncomeTaxExpense,
-            NetProfit = dto.NetIncome,
-            EarningsPerShare = dto.Eps,
-            EarningsPerShareDiluted = dto.EpsDiluted
-        };
+        
 
     private async Task AddBalanceSheetStatements(StockDto stock, CancellationToken cancellationToken)
     {
@@ -105,68 +98,60 @@ public class DatabaseWriter(
         var company = await dbContext.Companies.SingleOrDefaultAsync(company => company.Symbol == stock.Symbol, cancellationToken)
             ?? new Company { Name = stock.Name, Symbol = stock.Symbol, Exchange = exchange };
 
-        foreach (var balanceSheetStatement in await fmpClient
-                    .GetBalanceSheetStatementsAsync(stock.Symbol, cancellationToken))
+        foreach (var dto in await fmpClient.GetBalanceSheetStatementsAsync(stock.Symbol, cancellationToken))
         {
             await dbContext.BalanceSheetStatements.AddAsync(
-                await ToDatabaseObject(dbContext, balanceSheetStatement, company, cancellationToken),
+                new BalanceSheetStatement
+                {
+                    Company = company,
+                    Date = DateOnly.ParseExact(dto.Date, "yyyy-MM-dd"),
+                    Currency = await GetCurrency(dbContext, dto.ReportedCurrency, cancellationToken),
+                    CashAndCashEquivalents = dto.CashAndCashEquivalents,
+                    ShortTermInvestments = dto.ShortTermInvestments,
+                    NetReceivables = dto.NetReceivables,
+                    Inventory = dto.Inventory,
+                    OtherCurrentAssets = dto.OtherCurrentAssets,
+                    TotalCurrentAssets = dto.TotalCurrentAssets,
+                    PropertyPlantEquipmentNet = dto.PropertyPlantEquipmentNet,
+                    Goodwill = dto.Goodwill,
+                    IntangibleAssets = dto.IntangibleAssets,
+                    LongTermInvestments = dto.LongTermInvestments,
+                    TaxAssets = dto.TaxAssets,
+                    OtherNonCurrentAssets = dto.OtherNonCurrentAssets,
+                    TotalNonCurrentAssets = dto.TotalNonCurrentAssets,
+                    OtherAssets = dto.OtherAssets,
+                    TotalAssets = dto.TotalAssets,
+                    AccountPayables = dto.AccountPayables,
+                    ShortTermDebt = dto.ShortTermDebt,
+                    TaxPayables = dto.TaxPayables,
+                    DeferredRevenue = dto.DeferredRevenue,
+                    OtherCurrentLiabilities = dto.OtherCurrentLiabilities,
+                    TotalCurrentLiabilities = dto.TotalCurrentLiabilities,
+                    LongTermDebt = dto.LongTermDebt,
+                    DeferredRevenueNonCurrent = dto.DeferredRevenueNonCurrent,
+                    DeferredTaxLiabilitiesNonCurrent = dto.DeferredTaxLiabilitiesNonCurrent,
+                    MinorityInterest = dto.MinorityInterest,
+                    CapitalLeaseObligations = dto.CapitalLeaseObligations,
+                    OtherNonCurrentLiabilities = dto.OtherNonCurrentLiabilities,
+                    TotalNonCurrentLiabilities = dto.TotalNonCurrentLiabilities,
+                    OtherLiabilities = dto.OtherLiabilities,
+                    TotalLiabilities = dto.TotalLiabilities,
+                    PreferredStock = dto.PreferredStock,
+                    CommonStock = dto.CommonStock,
+                    RetainedEarnings = dto.RetainedEarnings,
+                    AccumulatedOtherComprehensiveIncomeLoss = dto.AccumulatedOtherComprehensiveIncomeLoss,
+                    OtherTotalStockholdersEquity = dto.OtherTotalStockholdersEquity,
+                    TotalStockholdersEquity = dto.TotalStockholdersEquity,
+                    TotalEquity = dto.TotalEquity,
+                    TotalInvestments = dto.TotalInvestments,
+                    TotalDebt = dto.TotalDebt,
+                    NetDebt = dto.NetDebt
+                },
                 cancellationToken);
         }
 
         await dbContext.SaveChangesAsync(cancellationToken);
-    }
-
-    private static async Task<BalanceSheetStatement> ToDatabaseObject(
-        SharePickerDbContext dbContext,
-        BalanceSheetStatementDto dto,
-        Company company,
-        CancellationToken cancellationToken) =>
-        new BalanceSheetStatement
-        {
-            Company = company,
-            Date = DateOnly.ParseExact(dto.Date, "yyyy-MM-dd"),
-            Currency = await GetCurrency(dbContext, dto.ReportedCurrency, cancellationToken),
-            CashAndCashEquivalents = dto.CashAndCashEquivalents,
-            ShortTermInvestments = dto.ShortTermInvestments,
-            NetReceivables = dto.NetReceivables,
-            Inventory = dto.Inventory,
-            OtherCurrentAssets = dto.OtherCurrentAssets,
-            TotalCurrentAssets = dto.TotalCurrentAssets,
-            PropertyPlantEquipmentNet = dto.PropertyPlantEquipmentNet,
-            Goodwill = dto.Goodwill,
-            IntangibleAssets = dto.IntangibleAssets,
-            LongTermInvestments = dto.LongTermInvestments,
-            TaxAssets = dto.TaxAssets,
-            OtherNonCurrentAssets = dto.OtherNonCurrentAssets,
-            TotalNonCurrentAssets = dto.TotalNonCurrentAssets,
-            OtherAssets = dto.OtherAssets,
-            TotalAssets = dto.TotalAssets,
-            AccountPayables = dto.AccountPayables,
-            ShortTermDebt = dto.ShortTermDebt,
-            TaxPayables = dto.TaxPayables,
-            DeferredRevenue = dto.DeferredRevenue,
-            OtherCurrentLiabilities = dto.OtherCurrentLiabilities,
-            TotalCurrentLiabilities = dto.TotalCurrentLiabilities,
-            LongTermDebt = dto.LongTermDebt,
-            DeferredRevenueNonCurrent = dto.DeferredRevenueNonCurrent,
-            DeferredTaxLiabilitiesNonCurrent = dto.DeferredTaxLiabilitiesNonCurrent,
-            MinorityInterest = dto.MinorityInterest,
-            CapitalLeaseObligations = dto.CapitalLeaseObligations,
-            OtherNonCurrentLiabilities = dto.OtherNonCurrentLiabilities,
-            TotalNonCurrentLiabilities = dto.TotalNonCurrentLiabilities,
-            OtherLiabilities = dto.OtherLiabilities,
-            TotalLiabilities = dto.TotalLiabilities,
-            PreferredStock = dto.PreferredStock,
-            CommonStock = dto.CommonStock,
-            RetainedEarnings = dto.RetainedEarnings,
-            AccumulatedOtherComprehensiveIncomeLoss = dto.AccumulatedOtherComprehensiveIncomeLoss,
-            OtherTotalStockholdersEquity = dto.OtherTotalStockholdersEquity,
-            TotalStockholdersEquity = dto.TotalStockholdersEquity,
-            TotalEquity = dto.TotalEquity,
-            TotalInvestments = dto.TotalInvestments,
-            TotalDebt = dto.TotalDebt,
-            NetDebt = dto.NetDebt
-        };
+    }        
 
     private async Task AddCashFlowStatements(StockDto stock, CancellationToken cancellationToken)
     {
@@ -181,52 +166,44 @@ public class DatabaseWriter(
         var company = await dbContext.Companies.SingleOrDefaultAsync(company => company.Symbol == stock.Symbol, cancellationToken)
             ?? new Company { Name = stock.Name, Symbol = stock.Symbol, Exchange = exchange };
 
-        foreach (var cashFlowStatement in await fmpClient
-                    .GetCashFlowStatementsAsync(stock.Symbol, cancellationToken))
+        foreach (var dto in await fmpClient.GetCashFlowStatementsAsync(stock.Symbol, cancellationToken))
         {
             await dbContext.CashFlowStatements.AddAsync(
-                await ToDatabaseObject(dbContext, cashFlowStatement, company, cancellationToken),
+                new CashFlowStatement
+                {
+                    Company = company,
+                    Date = DateOnly.ParseExact(dto.Date, "yyyy-MM-dd"),
+                    Currency = await GetCurrency(dbContext, dto.ReportedCurrency, cancellationToken),
+                    NetIncome = dto.NetIncome,
+                    DepreciationAndAmortisation = dto.DepreciationAndAmortization,
+                    DeferredIncomeTax = dto.DeferredIncomeTax,
+                    StockBasedCompensation = dto.StockBasedCompensation,
+                    AccountsReceivables = dto.AccountsReceivables,
+                    Inventory = dto.Inventory,
+                    AccountsPayables = dto.AccountsPayables,
+                    OtherWorkingCapital = dto.OtherWorkingCapital,
+                    ChangeInWorkingCapital = dto.ChangeInWorkingCapital,
+                    OtherNonCashItems = dto.OtherNonCashItems,
+                    NetCashFromOperations = dto.NetCashProvidedByOperatingActivities,
+                    CapitalExpenditure = dto.CapitalExpenditure,
+                    Acquisitions = dto.AcquisitionsNet,
+                    PurchasesOfInvestments = dto.PurchasesOfInvestments,
+                    SaleOrMaturityOfInvestments = dto.SalesMaturitiesOfInvestments,
+                    OtherInvestingActivities = dto.OtherInvestingActivites,
+                    NetCashFromInvesting = dto.NetCashUsedForInvestingActivites,
+                    SharesIssued = dto.CommonStockIssued,
+                    SharesRepurchased = dto.CommonStockRepurchased,
+                    DebtRepayment = dto.DebtRepayment,
+                    DividendsPaid = dto.DividendsPaid,
+                    OtherFinancingActivities = dto.OtherFinancingActivites,
+                    NetCashFromFinancing = dto.NetCashUsedProvidedByFinancingActivities,
+                    NetChangeInCash = dto.NetChangeInCash
+                },
                 cancellationToken);
         }
 
         await dbContext.SaveChangesAsync(cancellationToken);
     }
-
-    private static async Task<CashFlowStatement> ToDatabaseObject(
-        SharePickerDbContext dbContext,
-        CashFlowStatementDto dto,
-        Company company,
-        CancellationToken cancellationToken) => 
-        new CashFlowStatement
-        {
-            Company = company,
-            Date = DateOnly.ParseExact(dto.Date, "yyyy-MM-dd"),
-            Currency = await GetCurrency(dbContext, dto.ReportedCurrency, cancellationToken),
-            NetIncome = dto.NetIncome,
-            DepreciationAndAmortisation = dto.DepreciationAndAmortization,
-            DeferredIncomeTax = dto.DeferredIncomeTax,
-            StockBasedCompensation = dto.StockBasedCompensation,
-            AccountsReceivables = dto.AccountsReceivables,
-            Inventory = dto.Inventory,
-            AccountsPayables = dto.AccountsPayables,
-            OtherWorkingCapital = dto.OtherWorkingCapital,
-            ChangeInWorkingCapital = dto.ChangeInWorkingCapital,
-            OtherNonCashItems = dto.OtherNonCashItems,
-            NetCashFromOperations = dto.NetCashProvidedByOperatingActivities,
-            CapitalExpenditure = dto.CapitalExpenditure,
-            Acquisitions = dto.AcquisitionsNet,
-            PurchasesOfInvestments = dto.PurchasesOfInvestments,
-            SaleOrMaturityOfInvestments = dto.SalesMaturitiesOfInvestments,
-            OtherInvestingActivities = dto.OtherInvestingActivites,
-            NetCashFromInvesting = dto.NetCashUsedForInvestingActivites,
-            SharesIssued = dto.CommonStockIssued,
-            SharesRepurchased = dto.CommonStockRepurchased,
-            DebtRepayment = dto.DebtRepayment,
-            DividendsPaid = dto.DividendsPaid,
-            OtherFinancingActivities = dto.OtherFinancingActivites,
-            NetCashFromFinancing = dto.NetCashUsedProvidedByFinancingActivities,
-            NetChangeInCash = dto.NetChangeInCash
-        };
 
     private static async Task<Currency> GetCurrency(
         SharePickerDbContext dbContext,
