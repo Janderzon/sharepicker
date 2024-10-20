403c20db42adca55dc2fdd9322af4d9d0d7b8cf1
James Anderson <james.anderson2@btinternet.com>
2024-10-20 15:38:30 +0100

    Cleanup company record

--- SharePicker/SharePicker/Models/Company.cs
+++ SharePicker/SharePicker/Models/Company.cs
@@ -1,34 +1,3 @@
 ﻿namespace SharePicker.Models;
 
-public record Company(
-    string Symbol,
-    string Name
-    //string Exchange,
-    //List<IncomeStatement> IncomeStatements,
-    //List<BalanceSheetStatement> BalanceSheetStatements,
-    //List<CashFlowStatement> CashFlowStatements
-    //List<Ratios> Ratios
-    )
-{
-    //public List<LabeledValue> GetEbitPerRevenueSeries() => Ratios
-    //    .Select(x => new LabeledValue(x.DateTimeOffset.ToString("yyyy"), x.EbitPerRevenue))
-    //    .ToList();
-
-    //public List<LabeledValue> GetRoceSeries() => Ratios
-    //    .Where(x => x.ReturnOnCapitalEmployed != null)
-    //    .Select(x => new LabeledValue(x.DateTimeOffset.ToString("yyyy"), x.ReturnOnCapitalEmployed!.Value))
-    //    .ToList();
-
-    //public List<LabeledValue> GetCapitalTurnoverSeries() => Ratios
-    //    .Where(x => x.ReturnOnCapitalEmployed != null)
-    //    .Select(x => new LabeledValue(x.DateTimeOffset.ToString("yyyy"), x.ReturnOnCapitalEmployed!.Value / x.EbitPerRevenue))
-    //    .ToList();
-
-    //public List<LabeledValue> GetRevenueSeries() => IncomeStatements
-    //    .Select(x => new LabeledValue(x.DateTimeOffset.ToString("yyyy"), x.Revenue))
-    //    .ToList();
-
-    //public List<LabeledValue> GetEbitSeries() => IncomeStatements
-    //    .Select(x => new LabeledValue(x.DateTimeOffset.ToString("yyyy"), x.Ebit))
-    //    .ToList();
-};
+public record Company(string Symbol, string Name);
